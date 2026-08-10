783bc2d9615e3e81d91e3fb6ca04200d2706eb77
soheybdz13-design <soheybdz13@gmail.com>
2026-08-10 01:52:24 +0100

    Remove Firebase configuration

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,36 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
+import {
+  getFirestore
+} from "firebase/firestore";
+
+import {
+  getStorage
+} from "firebase/storage";
+
+import {
+  getAuth
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvRMDphiv7QswpOtq31Gn1u4I6E9aCEYg",
-  authDomain: "bm-sciences-ecfda.firebaseapp.com",
-  projectId: "bm-sciences-ecfda",
-  storageBucket: "bm-sciences-ecfda.firebasestorage.app",
-  messagingSenderId: "954410556995",
-  appId: "1:954410556995:web:024576db58076f965dfef6",
-  measurementId: "G-EQ197EVNNZ"
+
+  apiKey:"YOUR_API_KEY",
+
+  authDomain:"YOUR_PROJECT.firebaseapp.com",
+
+  projectId:"YOUR_PROJECT",
+
+  storageBucket:"YOUR_PROJECT.appspot.com",
+
+  messagingSenderId:"XXXXXXXX",
+
+  appId:"XXXXXXXX"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const auth = getAuth(app);
+export const db=getFirestore(app);
 
-export default app;
+export const storage=getStorage(app);
+
+export const auth=getAuth(app);
