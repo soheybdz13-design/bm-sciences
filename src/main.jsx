a997d4976588d451bc82e9d8486a5601c4d7e373
soheybdz13-design <soheybdz13@gmail.com>
2026-08-10 02:19:59 +0100

    Fix router path for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/bm-sciences">
+    <BrowserRouter>"
       <App />
     </BrowserRouter>
   </React.StrictMode>
